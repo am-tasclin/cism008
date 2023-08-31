3d34390188c18fc14db2b35588f915ebabd23b74
romanm <roman.mishchenko@gmail.com>
2023-08-31 21:17:20 +0300

    await fetch('./rs01')

--- src/main/resources/static/i.js
+++ src/main/resources/static/i.js
@@ -3,12 +3,24 @@ const uri_test01 = "ws://" + window.location.host + "/test01"
 console.log(uri_test01)
 const ws = new WebSocket(uri_test01)
 
+const testJson = { x: 1, y: 3 }
+testJson.sql = 'SELECT doc_id, parent p, reference r, reference2 r2, value vl_str, sort FROM doc \n\
+LEFT JOIN string ON doc_id=string_id \n\
+LEFT JOIN sort ON doc_id=sort_id \n\
+WHERE  376632 IN (doc_id,parent)'
+
 ws.onopen = event1 => {
-    ws.send(JSON.stringify({ x: 1, y: 2 }))
+    ws.send(JSON.stringify(testJson))
     // ws.send({ x: 1, y: 2 })
     return new Promise((thenFn) => ws.onmessage = event => thenFn(event)
     ).then(event => {
         console.log(event, 123)
-        console.log(event.data, 234)
+        const json = JSON.parse(event.data)
+        console.log(JSON.stringify(json, '', 2))
+        console.log(json.sql)
     })
 }
+
+(await fetch('./rs01')).json().then(json => {
+    console.log(json, 123)
+})
