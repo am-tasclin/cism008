9dce117496742e63e6c5c273489ef547ce0b62c8
romanm <roman.mishchenko@gmail.com>
2023-09-12 12:30:50 +0300

    ⛋ 𝑓 𝑡

--- src/main/resources/static/f/7/libWF/WfElement.js
+++ src/main/resources/static/f/7/libWF/WfElement.js
@@ -9,14 +9,26 @@ export default {
     mounted() { domConfWf().wfComponent[this.adnid] = this }, methods: {
         adn(adnId) { return mcData.eMap[adnId] || {} },
         parentChilds(adnId) { return mcData.parentChilds[adnId] || [] },
+        wfSymbolR(adnId) { return wfType[this.adn(adnId).r] },
+        wfSymbolR2(adnId) { return wfType[this.adn(adnId).r2] },
     }, template: `
 <div v-if="parentChilds(adnid).length" class="w3-container w3-border-left">
-    <span class="w3-tiny w3-opacity w3-right"> {{adnid}} {{parentChilds(adnid)}} </span>
-    <div v-for="adnId2 in parentChilds(adnid)" class="w3-hover-shadow">
-        <span class="w3-tiny"> {{adnId2}} </span>
-        {{adn(adnId2).vl_str}}
+    <template v-for="adnId2 in parentChilds(adnid)">
+        <div class="w3-hover-shadow">
+            <span class="w3-tiny">{{adnId2}}&nbsp;{{wfSymbolR(adnId2)}}{{wfSymbolR2(adnId2)}}
+            </span> {{adn(adnId2).vl_str}}
+            <span class="w3-right w3-tiny w3-opacity">
+            {{adn(adnId2).r}}|{{adn(adnId2).r2}}
+            </span>
+        </div>
         <t-wf :adnid="adnId2"></t-wf>
-    </div>
+    </template>
 </div><span class="w3-hide">{{count}}</span>
 `,
 }
+/**
+ * ⛋ -- Process in PlanDefinition
+ * 𝑓 -- ActivityDefinition
+ * 𝑡 -- Task
+ */
+const wfType = { 369782: '⛋', 371575: '⛋', 373500: '𝑓→', 371927: '𝑡→' }
