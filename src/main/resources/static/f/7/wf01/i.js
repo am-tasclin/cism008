e63f3ca3067bcbc64754aee663650de8fc4f5c2c
romanm <roman.mishchenko@gmail.com>
2023-09-15 15:28:59 +0300

    import { initWorkFlow } from '/f/7/wf01/libWF.js'

--- src/main/resources/static/f/7/wf01/i.js
+++ src/main/resources/static/f/7/wf01/i.js
@@ -4,75 +4,21 @@
  * 
  */
 import {
-    initDomConfLogic, mcData, setDomComponent, getDomComponent,
-    domConstants
+    initDomConfLogic, mcData, setDomComponent,
 } from '/f/7/libDomGrid/libDomGrid.js'
-import { readAdnByIds, readAdnByParentIds, initNamedSql } from '/f/7/libDbRw/libMcRDb.js'
+import { initWorkFlow } from '/f/7/wf01/libWF.js'
 
 // console.log(window.location.hash.substring(1))
 const domConf = initDomConfLogic(window.location.hash.substring(1))
-const uniqueIdsForDbRead = domConf.wf.l
-console.log(domConf, uniqueIdsForDbRead)
+console.log(domConf,)
 
 import { ws } from '/f/7/libDbRw/wsDbRw.js'
-ws.onopen = event =>
-    uniqueIdsForDbRead.length && readAdnByIds(uniqueIdsForDbRead
-    ).then(() => deepN_readParent(deepNum, uniqueIdsForDbRead, [], readTasks))
+ws.onopen = event => initWorkFlow()
 
-const readTasks = (x, deepCount) => {
-    // console.log('afterDeepN --> ', x, deepCount, mcData, domConstants)
-    const taskList = Object.keys(mcData.eMap).reduce((l, i) => domConstants.taskElementList
-        .includes(mcData.eMap[i].r) && l.push(mcData.eMap[i].r2) && l || l, [])
-    // console.log(deepCount, taskList)
-    taskList.length && readAdnByIds(taskList
-    ).then(() => deepN_readParent(deepNum, taskList, [], afterReadTasks))
-}
 const codeMetaData = [368597, 367562,]
-const codeRepresentation = [377146,]
-const codes = codeMetaData.concat(codeRepresentation)
-const afterReadTasks = (x, deepCount) => {
-    // console.log(mcData.eMap[377155], mcData.parentChilds[377155])
-    // console.log(mcData.eMap[377156], mcData.parentChilds[377156])
-    // console.log(34, codes, initNamedSql({ n: 'selectDocVlStrByParentIds', l: [377108] }))
-    readAdnByIds(codes).then(() =>
-        deepN_readParent(deepNum, codes, [], afterReadCodes))
-}
-const loggedAttributes = [372052, 377121, 377149, 377170]
-const afterReadCodes = (x, deepCount) => {
-    console.log(deepCount, mcData)
-    readAdnByIds(loggedAttributes).then(() => {
-        getDomComponent('cmd').count++
-        getDomComponent('ccr').count++
-        console.log(377121, mcData.eMap[377121])
-        console.log(domConf.wf.taskComponent)
-        Object.keys(domConf.wf.taskComponent).forEach(i =>
-            domConf.wf.taskComponent[i].count++)
-    })
-}
-
-const deepNum = 6
-const deepN_readParent = (deepCount, list, prevList, fn) => {
-    // console.log(deepCount, list, list.length)
-    // console.log(deepCount, deepCount == 0, !list.length, deepCount == 0 || !list.length);
-    (deepCount == 0 || !list.length) && fn(list, deepCount)
-    deepCount > 0 && list.length && readAdnByParentIds(list).then(() => {
-        getDomComponent('workFlow').count++
-        getDomComponent('wf01').count++
-        codes[0] == prevList[0] && getDomComponent('ccr').count++
-        codes[0] == prevList[0] && getDomComponent('cmd').count++
-        list.forEach(i => domConf.wf.taskComponent[i] &&
-            domConf.wf.taskComponent[i].count++)
-        prevList.find(i => domConf.wf.taskComponent[i] && mcData.parentChilds[i] &&
-            domConf.wf.taskComponent[i].count++)
-
-        list.forEach(i => domConf.wf.wfComponent[i] && domConf.wf.wfComponent[i].count++)
-        list.forEach(i => mcData.eMap[i] && domConf.wf.wfComponent[mcData.eMap[i].p] &&
-            domConf.wf.wfComponent[mcData.eMap[i].p].count++)
-        const newList = Object.keys(mcData.eMap).filter(i => !mcData.parentChilds[i] && !prevList.includes(i))
-        // console.log(deepCount, newList, newList.length)
-        deepN_readParent(--deepCount, newList, list.concat(newList), fn)
-    })
-}
+    , codeRepresentation = [377146,]
+domConf.wf.codes = codeMetaData.concat(codeRepresentation)
+domConf.wf.loggedAttributes = [372052, 377121, 377149, 377170]
 
 const { createApp } = Vue
 const wf01 = createApp({
